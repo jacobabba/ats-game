c638e4f15bc9f42f0bd34abea59da8b0279308e6
jacobabba <jacobabba@gmail.com>
2016-04-24 02:20:07 -0700

    Floored draw coordinates in the draw system to see how it affects stutter

--- systems/draw_system.lua
+++ systems/draw_system.lua
@@ -24,24 +24,24 @@ do
                 else
                     love.graphics.setColor(255, 255, 255)
                 end
-                love.graphics.rectangle("fill", x, y, 
+                love.graphics.rectangle("fill", math.floor(x), math.floor(y), 
                                         v.transform.width, v.transform.height)
             elseif v.drawable.drawType == "line" then
                 love.graphics.setColor(COLOR_CODES[v.colorState.state])
                 for _,segv in ipairs(v.line.segments) do
                     if segv.width then
                         love.graphics.line(
-                            (segv.x-1)*TILE_SIZE+world.shiftX,
-                            (segv.y-.5)*TILE_SIZE+world.shiftY,
-                            (segv.x-1+segv.width)*TILE_SIZE+world.shiftX,
-                            (segv.y-.5)*TILE_SIZE+world.shiftY
+                            math.floor((segv.x-1)*TILE_SIZE+world.shiftX),
+                            math.floor((segv.y-.5)*TILE_SIZE+world.shiftY),
+                            math.floor((segv.x-1+segv.width)*TILE_SIZE+world.shiftX),
+                            math.floor((segv.y-.5)*TILE_SIZE+world.shiftY)
                         )
                     elseif segv.height then
                         love.graphics.line(
-                            (segv.x-.5)*TILE_SIZE+world.shiftX,
-                            (segv.y-1)*TILE_SIZE+world.shiftY,
-                            (segv.x-.5)*TILE_SIZE+world.shiftX,
-                            (segv.y-1+segv.height)*TILE_SIZE+world.shiftY
+                            math.floor((segv.x-.5)*TILE_SIZE+world.shiftX),
+                            math.floor((segv.y-1)*TILE_SIZE+world.shiftY),
+                            math.floor((segv.x-.5)*TILE_SIZE+world.shiftX),
+                            math.floor((segv.y-1+segv.height)*TILE_SIZE+world.shiftY)
                         )
                     end
                 end
